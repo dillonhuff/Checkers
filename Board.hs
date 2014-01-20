bca0136b13cdd406ae3d2890aace687cc8f0df91
Dillon Huff <dillonhuff@gmail.com>
2014-01-20 04:52:18 -0800

    working on Board typeclass

--- Board.hs
+++ Board.hs
@@ -1,2 +1,16 @@
 module Board() where
 
+data Square = Empty | Piece Player PieceType
+
+data Player = Red | Black
+
+data PieceType = Reg | King
+
+data Move = M [(Int, Int)]
+
+class Board b where
+	--Gets the contents of a square
+	s :: (Integral a) => b -> a -> a -> Square
+	legalMoves :: b -> Player -> [Move]
+	move :: b -> Move -> b
+	
