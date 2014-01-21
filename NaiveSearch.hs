e78277e7bc2dc098e298ed698b530e33e9e18adf
Dillon Huff <dillonhuff@gmail.com>
2014-01-21 13:14:13 -0800

    starting work on GameTree generation

--- NaiveSearch.hs
+++ NaiveSearch.hs
@@ -10,4 +10,7 @@ pieceDiff :: (Board b) => b -> Player -> Float
 pieceDiff b p = fromIntegral (playerPieces - otherPieces)
 	where
 		playerPieces = (length (kings b p)) + (length (regularPieces b p))
-		otherPieces = (length (kings b (otherPlayer p))) + (length (regularPieces b (otherPlayer p)))
+		otherPieces = (length (kings b (otherPlayer p)))
+			+ (length (regularPieces b (otherPlayer p)))
+			
+data GameTree = GT Float [GameTree]
