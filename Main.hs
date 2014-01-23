1c7e143fb021a805d7b5e63b86ec2652424034e3
Dillon Huff <dillonhuff@gmail.com>
2014-01-23 12:13:37 -0800

    cleaned up human turn code a bit

--- Main.hs
+++ Main.hs
@@ -12,17 +12,16 @@ playGame = humanTurn startingBoard (push (square 0 0) (square 0 0)) Black
 			
 humanTurn :: MapBoard -> Move -> Player -> IO ()
 humanTurn b m p = if (canJumpAgain b m p)
-	then jumpMove b m p
-	else unrestrictedMove b (otherPlayer p)
+	then doTurn b (jumpsFromLastMove b p m) p
+	else doTurn b (legalMoves b (otherPlayer p)) (otherPlayer p)
 
 canJumpAgain :: MapBoard -> Move -> Player -> Bool
 canJumpAgain b m p = if isJump m
 	then length (jumpsFromLastMove b p m) > 0
 	else False
 	
-jumpMove :: MapBoard -> Move -> Player -> IO ()
-jumpMove b m p = do
-	let moves = jumpsFromLastMove b p m
+doTurn :: MapBoard -> [Move] -> Player -> IO ()
+doTurn b moves p = do
 	putStrLn (show b ++ "\n" ++ "Enter the number of your move\n")
 	putStrLn (showMoves 1 moves)
 	moveNum <- getLine
@@ -30,20 +29,7 @@ jumpMove b m p = do
 	putStrLn ("You selected " ++ show selectedMove ++ "\n")
 	let boardAfterMove = move b selectedMove
 	case winner boardAfterMove of
-		Just winningPlayer -> putStrLn (show winningPlayer ++ " wins\n")
-		Nothing -> humanTurn boardAfterMove selectedMove p
-	
-unrestrictedMove :: MapBoard -> Player -> IO ()
-unrestrictedMove b p = do
-	let moves = legalMoves b p
-	putStrLn (show b ++ "\n" ++ "Enter the number of your move\n")
-	putStrLn (showMoves 1 moves)
-	moveNum <- getLine
-	let selectedMove = (moves !! ((read moveNum) - 1))
-	putStrLn ("You selected " ++ show selectedMove ++ "\n")
-	let boardAfterMove = move b selectedMove
-	case winner boardAfterMove of
-		Just winningPlayer -> putStrLn (show winningPlayer ++ " wins\n")
+		Just winningPlayer -> putStrLn (show winningPlayer ++ " wins\n" ++ show boardAfterMove)
 		Nothing -> humanTurn boardAfterMove selectedMove p
 					
 showMoves :: Int -> [Move] -> String
