31c88befdd20561af4814e6b216b50e2ccf5f349
Dillon Huff <dillonhuff@gmail.com>
2014-01-24 09:00:59 -0800

    working on converting movetree to move sequences tree

--- NaiveSearch.hs
+++ NaiveSearch.hs
@@ -14,7 +14,26 @@ pieceDiff p b = fromIntegral (playerPieces - otherPieces)
 		otherPieces = (length (kings b (otherPlayer p)))
 			+ (length (regularPieces b (otherPlayer p)))
 	
-data MoveTree = Root [MoveTree] | MTree Move [MoveTree]
+data MoveSeqTree = RootSeq [MoveSeqTree] | MSTree [Move] [MoveSeqTree]
+	deriving (Show)
+	
+makeMoveSeqTree :: Int -> MapBoard -> Player -> MoveSeqTree
+makeMoveSeqTree depth b p = moveToMoveSeq $ makeMoveTree depth b p
+
+moveToMoveSeq :: MoveTree -> MoveSeqTree
+moveToMoveSeq (Root children) = RootSeq $ map moveToMoveSeq children
+moveToMoveSeq (MTree (p, m) []) = MSTree [m] []
+moveToMoveSeq (MTree (p, m) children) = if (not (isJump m))
+	then MSTree [m] $ map moveToMoveSeq children
+	else jumpSequence p [m] children
+
+jumpSequence :: Player -> [Move] -> [MoveTree] -> MoveSeqTree
+jumpSequence p moves [] = MSTree moves []
+jumpSequence p moves children@((MTree (pl, move) _):rest) = if (pl == p) && (isJump move)
+	then MSTree [] []
+	else MSTree moves $ map moveToMoveSeq children
+
+data MoveTree = Root [MoveTree] | MTree (Player, Move) [MoveTree]
 	deriving (Show)
 
 makeMoveTree :: Int -> MapBoard -> Player -> MoveTree
@@ -23,9 +42,9 @@ makeMoveTree depth board player = Root $ map (turn depth board player) (legalMov
 turn :: (Board b) => Int -> b -> Player -> Move -> MoveTree
 turn depth b p m = if (depth > 0) 
 	then if (canJumpAgain b m p)
-		then MTree m $ map (turn (depth-1) boardAfterMove p) (jumpsFromLastMove boardAfterMove p m)
-		else MTree m $ map (turn (depth-1) boardAfterMove nextPlayer) (legalMoves boardAfterMove nextPlayer)
-	else MTree m []
+		then MTree (p, m) $ map (turn (depth-1) boardAfterMove p) (jumpsFromLastMove boardAfterMove p m)
+		else MTree (p, m) $ map (turn (depth-1) boardAfterMove nextPlayer) (legalMoves boardAfterMove nextPlayer)
+	else MTree (p, m) []
 	where
 		boardAfterMove = move b m
 		nextPlayer = otherPlayer p
