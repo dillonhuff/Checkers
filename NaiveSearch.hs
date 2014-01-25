89d434b9a3cc75bf9e0d74c68ccf50ebb73a930f
Dillon Huff <dillonhuff@gmail.com>
2014-01-25 03:44:08 -0800

    some progress on naive search

--- NaiveSearch.hs
+++ NaiveSearch.hs
@@ -17,24 +17,21 @@ pieceDiff p b = fromIntegral (playerPieces - otherPieces)
 data MoveSeqTree = RootSeq [MoveSeqTree] | MSTree [Move] [MoveSeqTree]
 	deriving (Show)
 	
-makeMoveSeqTree :: Int -> MapBoard -> Player -> MoveSeqTree
-makeMoveSeqTree depth b p = moveToMoveSeq $ makeMoveTree depth b p
+moveSequence :: MoveTree -> [MoveSeqTree]
+moveSequence (Root children) = [RootSeq $ concat $ map moveSequence children]
+moveSequence (MTree (p, m) children) = if (not (isJump m))
+	then [MSTree [m] $ concat $ map moveSequence children]
+	else concat $ map (jumpSequence p [m]) children
 
-moveToMoveSeq :: MoveTree -> MoveSeqTree
-moveToMoveSeq (Root children) = RootSeq $ map moveToMoveSeq children
-moveToMoveSeq (MTree (p, m) []) = MSTree [m] []
-moveToMoveSeq (MTree (p, m) children) = if (not (isJump m))
-	then MSTree [m] $ map moveToMoveSeq children
-	else jumpSequence p [m] children
-
-jumpSequence :: Player -> [Move] -> [MoveTree] -> MoveSeqTree
-jumpSequence p moves [] = MSTree moves []
-jumpSequence p moves children@((MTree (pl, move) _):rest) = if (pl == p) && (isJump move)
-	then MSTree [] []
-	else MSTree moves $ map moveToMoveSeq children
+jumpSequence :: Player -> [Move] -> MoveTree -> [MoveSeqTree]
+jumpSequence p moves (MTree (pl, move) children) = if (pl == p) && (isJump move)
+	then if (children == [])
+		then [MSTree (moves ++ [move]) []]
+		else concat $ map (jumpSequence p (moves ++ [move])) children
+	else [MSTree moves (concat $ map moveSequence children)]
 
 data MoveTree = Root [MoveTree] | MTree (Player, Move) [MoveTree]
-	deriving (Show)
+	deriving (Show, Eq)
 
 makeMoveTree :: Int -> MapBoard -> Player -> MoveTree
 makeMoveTree depth board player = Root $ map (turn depth board player) (legalMoves board player)
